1049a089b04f6b6c244569a33e5d6db8ad34c979
Anhnhi Tran <anhtran811@gmail.com>
2022-12-19 20:17:56 -0500

    Add avg contestant experience to show spec and view

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -52,4 +52,47 @@ RSpec.describe "Project's show page" do
       expect(page).to have_content("Number of contestants: 3")
     end
   end
+
+# As a visitor,
+# When I visit a project's show page
+# I see the average years of experience for the contestants that worked on that project
+# (e.g.    Litfit
+#     Material: Lamp Shade
+#   Challenge Theme: Apartment Furnishings
+#   Number of Contestants: 3
+#   Average Contestant Experience: 10.25 years)
+  describe 'Extension 1' do
+    it 'can show the average years of experience for the contestants by project' do
+      jay = Contestant.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create!(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+      ContestantProject.create!(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create!(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create!(contestant_id: kentaro.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: jay.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+
+      visit "/projects/#{news_chic.id}"
+      
+      within("#avg_contestant_exp-#{news_chic.id}") do
+        expect(news_chic.contestants.avg_contestant_experience).to eq(11.0)
+      end
+
+      visit "/projects/#{upholstery_tux.id}"
+      
+      within("#avg_contestant_exp-#{upholstery_tux.id}") do
+        expect(upholstery_tux.contestants.avg_contestant_experience).to eq(10.5)
+      end
+
+      expect(page).to have_content("Average Contestant Experience:")
+    end
+  end
+
 end
