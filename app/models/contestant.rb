9e3855574de965eca42d77a0667ebe26d583a765
Anhnhi Tran <anhtran811@gmail.com>
2022-12-19 20:17:34 -0500

    Add model test for avg contestant experience with passing test

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,9 @@ class Contestant < ApplicationRecord
   validates_presence_of :name, :age, :hometown, :years_of_experience
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
+
+  def self.avg_contestant_experience
+    average(:years_of_experience).to_f.round(2)
+  end
 end
+
--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe "#avg_contestant_experience" do
+    it 'can find the avg contestant experience' do
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    
+      expect(Contestant.avg_contestant_experience).to eq(11.0)
+    end
+  end
 end
